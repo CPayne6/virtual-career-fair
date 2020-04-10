8d6cbcc68856adc14c9ce49aa9fcc9cbc569b9a9
James McDonald <james_mcd98@hotmail.com>
2020-04-09 22:09:58 -0400

    Added mongodb connection and User Model example

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+
 
 require('dotenv').config(); // Import environment variables
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
+
+mongoose
+    .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> console.log("Connected to mongoDB "))
+    .catch(err => console.log(err));
 
 app.listen(process.env.PORT, () => {console.log(`app running on port ${process.env.PORT}`)});
