501a9c25deaa1297f17bb7db1f00131cb8662530
CPayne6 <cpaynetrayne6@gmail.com>
2020-04-15 20:01:13 -0400

    placeholder

--- api/router/account/recruiter.js
+++ api/router/account/recruiter.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const recruiterAuth = require('../../middleware/auth/recruiter');
 
+const signin = require('../../../placeholder/sign-in').recruiter;
+const signup = require('../../../placeholder/sign-up').recruiter;
+
 const router = express.Router();    //  router operating on path '/api/account/recruiter/login'
 
 router.post('/login', (req, res) => {
-    res.send('login'); //  Temporary response for test
+    res.send(signin); //  Temporary response for test
 });
 
 router.post('/signup', (req, res) => {
-    res.send('signup'); //  Temporary response for test
+    res.send(signup); //  Temporary response for test
 });
 
 router.post('/logout', () => {
-    res.send('logout'); //  Temporary response for test
+    res.send({success: true}); //  Temporary response for test
 });
 
 router.use(recruiterAuth);
--- api/router/account/user.js
+++ api/router/account/user.js
@@ -4,22 +4,23 @@ const cookie = require('../../../util/cookies');
 const userService = require('../../controllers/user');
 const userModel = require('../../../db/models/User');
 
+// placeholder routes
+const signin = require('../../../placeholder/sign-in').user;
+const signup = require('../../../placeholder/sign-up').user;
+
 const router = express.Router();    //  router operating on path '/api/account/user/login'
 
 router.post('/login', (req, res) => {
-  let response = require('../../../placeholder/sign-in').user;
-  cookie(req.user, res);
-  res.send(response); //  Temporary response for test
+  res.send(signin); //  Temporary response for test
 });
 
 router.post('/signup', (req, res) => {
-  userService.signup(req, res);
-  let response = require('../../../placeholder/sign-in').user;
-  res.send(response); //  Temporary response for test
+  //userService.signup(req, res);
+  res.send(signup); //  Temporary response for test
 });
 
 router.post('/logout', () => {
-  res.send('logout'); //  Temporary response for test
+  res.send({success: true}); //  Temporary response for test
 });
 
 router.use(userAuth);
